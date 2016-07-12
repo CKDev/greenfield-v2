05e7def1b65bb4e982e0db74ed0eb2f06d1d779c
Alex Brinkman <abrink@commercekitchen.com>
2016-07-12 10:52:49 -0600

    [CONFIG] Update project to use Rspec

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,14 @@ module Greenfield
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Preferred generator overrides
+    config.generators do |g|
+      g.test_framework :rspec
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+      g.stylesheets false
+      g.javascripts false
+      g.helpers false
+    end
   end
 end
---
+++ spec/feature_helper.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+require "capybara/rails"
+require "capybara/rspec"
+
+def log_in_with(email, password)
+  visit new_session_path
+  find("#user_email").set(email)
+  find("#user_password").set(password)
+  click_button "Let's Go!"
+end
+
+def log_in(user)
+  visit root_path
+  click_link "Log In"
+  find("#user_email", visible: false).set(user.email)
+  find("#user_password", visible: false).set(user.password)
+  click_button "Log in"
+end
+
+def log_out
+  click_link "Log Out"
+end
+
+Capybara.javascript_driver = :webkit
+# Capybara.javascript_driver = :selenium
+Capybara::Webkit.configure do |config|
+  # config.debug = true # Uncomment for error information.
+end
+
+Capybara.configure do |config|
+  config.always_include_port = true
+end
